013f694bbe9c665c9ec89468abede8a71267a9c8
Gustavo-F-E <112873809+Gustavo-F-E@users.noreply.github.com>
2023-04-04 11:47:45 -0300

    first commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('perros/', views.perros, name='perros'),
+    path('gatos/', views.gatos, name='gatos'),
+    path('servicios/', views.servicios, name='servicios'),
+]
---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import core
+
+# Create your views here.
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def perros(request):
+    template = loader.get_template('perros.html')
+    return HttpResponse(template.render())
+
+def gatos(request):
+    template = loader.get_template('gatos.html')
+    return HttpResponse(template.render())
+
+def servicios(request):
+    template = loader.get_template('servicios.html')
+    return HttpResponse(template.render())
