53d5f03378a67a54c9d4f7e507990bb448753231
Daniel Espinosa <control.de@gmail.com>
2023-04-13 00:37:27 -0300

    setting

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(vx80=-)ilq110s@x-gk67h^_wcb%0y-6m51ae$*6vhl7q*xu&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
