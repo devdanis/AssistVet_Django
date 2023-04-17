37ce38198894a0bed073ec3c7c271835382bef6f
Daniel Espinosa <control.de@gmail.com>
2023-04-17 10:37:52 -0300

    Actualizado html con static - funcionando

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('perros/', views.perros, name='perros'),
     path('gatos/', views.gatos, name='gatos'),
     path('servicios/', views.servicios, name='servicios'),
--- tienda/views.py
+++ tienda/views.py
@@ -6,17 +6,17 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    #template = loader.get_template('index.html')
+    return render(request, 'tienda/index.html')
 
 def perros(request):
-    template = loader.get_template('perros.html')
-    return HttpResponse(template.render())
+    #template = loader.get_template('perros.html')
+    return render(request, 'tienda/perros.html')
 
 def gatos(request):
-    template = loader.get_template('gatos.html')
-    return HttpResponse(template.render())
+    #template = loader.get_template('gatos.html')
+    return render(request, 'tienda/gatos.html')
 
 def servicios(request):
-    template = loader.get_template('servicios.html')
-    return HttpResponse(template.render())
+    #template = loader.get_template('servicios.html')
+    return render(request, 'tienda/servicios.html')
